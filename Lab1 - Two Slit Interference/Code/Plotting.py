859bf6ee70b1548188f949d11bf7b8134d91d7a3
Thresa Kelly <polaris133pc@gmail.com>
2023-09-14 13:31:07 -0500

    move functions to py

--- Lab1 - Two Slit Interference/Code/Plotting.py
+++ Lab1 - Two Slit Interference/Code/Plotting.py
@@ -1,6 +1,8 @@
 # all imports
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+import numpy as np
 
 # text sizes
 BIG = 16
@@ -33,3 +35,67 @@ def Save(filename) :
                     facecolor='w',
                     edgecolor='w'
     )
+
+def PlotXY(
+    x: np.array,        # x axis data points
+    y: np.array,        # y axis data points
+    xlab: str,          # label on x axis 
+    ylab: str,          # label on y axis
+    color = 'k',        # color of the line 
+    xlim: list = None,  # x axis range 
+    ylim: list = None,  # y axis range 
+    fileName: str = ''  # filename to save the figure as
+) : 
+    """Basic plotting function for x and y data arrays."""
+    # formatting 
+    SetStyle()
+    plt.xlabel(xlab)
+    plt.ylabel(ylab)
+    if(xlim != None) : plt.xlim(xlim)
+    if(ylim != None) : plt.ylim(ylim)
+    ax = plt.gca()
+    ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+    ax.yaxis.set_minor_locator(AutoMinorLocator(4))
+    ax.grid()
+    # data
+    plt.plot(x,y,color=color)
+    # finish 
+    if(fileName != '') : Save(fileName)
+    plt.show()
+    plt.close()
+
+def PlotMultiXY(
+    x: list[np.array],  # x axis data points
+    y: list[np.array],  # y axis data points
+    xlab: str,          # label on x axis 
+    ylab: str,          # label on y axis
+    color: list,        # color of the line 
+    legend: list,       # lables for xy pair on legend
+    xlim: list = None,  # x axis range
+    ylim: list = None,  # y axis range
+    hideY: bool = False,
+    fileName: str = ''  # filename to save the figure as
+) : 
+    """Basic plotting function for x and y data arrays."""
+    # formatting 
+    SetStyle()
+    plt.xlabel(xlab)
+    plt.ylabel(ylab)
+    if(xlim != None) : plt.xlim(xlim)
+    if(ylim != None) : plt.ylim(ylim)
+    if(hideY) : plt.tick_params(axis='y', which='both', labelleft=False)
+    ax = plt.gca()
+    ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+    ax.yaxis.set_minor_locator(AutoMinorLocator(4))
+    ax.grid()
+    # data
+    for xl,yl,cl,ll in zip(x,y,color,legend) : 
+        plt.plot(xl, yl,
+                 color=cl, alpha=0.7,
+                 linestyle='-', linewidth=1,
+                 label=ll)
+    plt.legend()
+    # finish 
+    if(fileName != '') : Save(fileName)
+    plt.show()
+    plt.close()
